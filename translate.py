6ded90f2aad847f3c91930a72d603da18c5abeb4
Vladimir-82 <61869738+Vladimir-82@users.noreply.github.com>
2022-10-14 15:32:34 +0300

    googletrans_

--- translate.py
+++ translate.py
@@ -1,8 +1,9 @@
+from googletrans import Translator
 import googletrans
 
 print(googletrans.LANGUAGES)
 
-translator = googletrans.Translator()
+translator = Translator()
 result = translator.translate('It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout', src='en', dest='be')
 
 print(result.src)
