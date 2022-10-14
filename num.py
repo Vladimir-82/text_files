e55b1c6845c9e70a84a68ee4c82e87286b4ccc77
Vladimir-82 <61869738+Vladimir-82@users.noreply.github.com>
2022-10-14 14:35:08 +0300

    googletrans

--- num.py
+++ num.py
@@ -1,13 +1,33 @@
-f = open('numbers.txt', 'r', encoding='utf-8')
-content = f.readlines()
-for i in content:
-    print(sum([int(j) for j in i.rstrip().split()]))
+NUM = {'IV': 4, 'IX': 9, 'X': 10, 'L': 50, 'C': 100, 'D': 500,
+       'M': 1000, 'I': 1, 'V': 5,
+       }
+
+def solution(roman):
+    res = ''
+    lst = []
+    while roman:
+        if roman[:2] in NUM.keys():
+            res += str(NUM[roman[:2]])
+            roman = roman[2:]
+        else:
+            lst.append(NUM[roman[:1]])
+            roman = roman[1:]
+    return res + str(sum(lst))
 
 
 
 
 
-with open('numbers.txt', 'r', encoding='utf-8') as file:
-    context = list(map(str.strip, file.readlines()))
-    for i in context:
-        print(sum(map(int, i.split())))
+
+print(solution('IVCIX'))
+
+
+
+# def solution(roman):
+#     res = 0
+#     for key, value in NUM.items():
+#         while key in roman:
+#             ind = roman.find(key)
+#             roman = roman[:ind] + roman[ind+len(key):]
+#             res += value
+#     return res
--- translate.py
+++ translate.py
@@ -1,6 +1,13 @@
-from googletrans import Translator
+import googletrans
 
-a = Translator()
-# a = Translator.translate('If source language is not given, google translate attempts to detect the source language.')
+print(googletrans.LANGUAGES)
 
-a.translate('If', dest='ja')
+translator = googletrans.Translator()
+result = translator.translate('It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout', src='en', dest='be')
+
+print(result.src)
+print(result.dest)
+print(result.text)
+
+# pip install googletrans==3.1.0a0
+
